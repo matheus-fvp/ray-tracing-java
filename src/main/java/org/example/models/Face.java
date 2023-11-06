853e478c6e7edd55711437fb5e15c926c75c2a13
Matheus Fernando Vieira Pinto <vieira.matheusf13@gmail.com>
2023-11-06 09:35:59 -0300

    add Vertex and Face classes

---
+++ src/main/java/org/example/models/Face.java
@@ -0,0 +1,30 @@
+package org.example.models;
+
+public class Face {
+
+    private int v1;
+    private int v2;
+    private int v3;
+
+    public Face() {
+    }
+
+    public Face(int v1, int v2, int v3) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+    }
+
+    public int getV1() {
+        return v1;
+    }
+
+    public int getV2() {
+        return v2;
+    }
+
+    public int getV3() {
+        return v3;
+    }
+
+}
